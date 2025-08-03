e47ee54a154623250dcca7d47b3fc8a08f6bfdbc
himanshuihm1619-lgtm <himanshuihm1619@gmail.com>
2025-08-03 22:50:15 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,37 @@
+'''
+
+ASSIGNMENT 1:
+Module 2: Basic Python Concepts
+
+Task 1: Perform Basic Mathematical Operations
+'''
+
+x = input("Enter the first number:  ")
+y = input("Enter the second number: ")
+x = float(x)
+y = float(y)
+
+add = x + y
+sub = x - y
+multi = x*y
+div = y/x
+
+print("Addition: ",add)
+print("Subtraction ",sub)
+print("Multiplication ",multi)
+print("Division ",div)
+
+
+# Task 2: Create a Personalized Greeting
+
+a = input("Enter your first name: ")
+a = str(a)
+b = input("Enter your last name: ")
+b  = str(b)
+
+Full_name = a + b
+
+print("Hello,",Full_name + "! Welcome to python program.")
+
+
+
